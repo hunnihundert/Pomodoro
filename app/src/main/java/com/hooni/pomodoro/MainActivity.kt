2074aa7cc88db1235809965ec869f93ba7a0a619
hunnihundert <47563806+hunnihundert@users.noreply.github.com>
2020-01-26 08:40:10 +0900

    - removed invisible navigation bar from settings screen - implemented translucent status & navigation bar programatically

--- app/src/main/java/com/hooni/pomodoro/MainActivity.kt
+++ app/src/main/java/com/hooni/pomodoro/MainActivity.kt
@@ -9,20 +9,17 @@ import android.media.RingtoneManager
 import android.net.Uri
 import android.os.*
 import android.provider.Settings
-import androidx.appcompat.app.AppCompatActivity
-import android.util.Log
 import android.util.TypedValue
-import android.view.Menu
-import android.view.MenuItem
-import android.view.View
-import android.view.WindowManager
+import android.view.*
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.os.BuildCompat
 import com.hooni.pomodoro.util.NotificationUtil
 import com.hooni.pomodoro.util.PrefUtil
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -141,8 +138,10 @@ class MainActivity : AppCompatActivity() {
     private fun hideNaviAndStatusBar() {
         window.decorView.apply {
             systemUiVisibility =
-                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_FULLSCREEN
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
         }
+        window.attributes.flags = WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION or WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS
+
     }
 
 
--- app/src/main/java/com/hooni/pomodoro/SettingsActivity.kt
+++ app/src/main/java/com/hooni/pomodoro/SettingsActivity.kt
@@ -2,12 +2,21 @@ package com.hooni.pomodoro
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.hooni.pomodoro.util.PrefUtil
 import kotlinx.android.synthetic.main.activity_settings.*
 
 class SettingsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        when (PrefUtil.getDarkMode(this)) {
+            AppConstants.LIGHT_MODE -> setTheme(R.style.lightTheme)
+            AppConstants.DARK_MODE -> setTheme(R.style.darkTheme)
+        }
+        window.decorView.apply {
+
+        }
+
         setContentView(R.layout.activity_settings)
         setSupportActionBar(toolBarSettings)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
